93be41a4996844caf06c4fb77830d266bb75051c
karolinamaras <274676273+karolinamaras@users.noreply.github.com>
2026-05-25 11:56:27 -0400

    Add docstrings for Recipe and RecipeRating models

--- app/models.py
+++ app/models.py
@@ -3,6 +3,15 @@ from django.conf import settings
 
 
 class Recipe(models.Model):
+    """
+    Represents a recipe with details, such as title, description, preparation time,
+    and price.
+
+    This class serves as a data model for creating and managing recipe entities,
+    including attributes such as the title of the recipe, a detailed description, the
+    time required to prepare it (in minutes), and its price. Instances of this class
+    are typically used in applications related to recipe management or cooking guidelines.
+    """
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
@@ -13,6 +22,14 @@ class Recipe(models.Model):
 
 
 class RecipeRating(models.Model):
+    """
+    Represents a user's rating for a specific recipe.
+
+    This model is used to store ratings given by users to recipes. Each rating is
+    associated with a recipe and a user. Ratings are expressed in stars ranging
+    from 1 to 5. The model ensures that each user can only rate a recipe once,
+    and ratings are ordered by creation date in descending order.
+    """
     recipe = models.ForeignKey(
         Recipe, on_delete=models.CASCADE, related_name="ratings"
     )
